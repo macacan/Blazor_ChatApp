541e342a8642b1f734d5aacc7ab9f80482bb82e1
Martin Can <martin.can@yh.nackademin.se>
2024-10-07 12:11:57 +0200

    commit items

--- Blazor_ChatApp/Blazor_ChatApp/obj/Debug/net8.0/Blazor_ChatApp.AssemblyInfo.cs
+++ Blazor_ChatApp/Blazor_ChatApp/obj/Debug/net8.0/Blazor_ChatApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blazor_ChatApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5aace6978440302149dee55da81e3c17be08a56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+367fdb23fccd1f0a903967911c3882ae199740cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blazor_ChatApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blazor_ChatApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
